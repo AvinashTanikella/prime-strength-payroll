eb02663d3c61ab5a16fd29caa98f65b3f051a50c
Avinash Tanikella <60061392+AvinashTanikella@users.noreply.github.com>
2026-03-19 01:19:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ nfp_df = pd.DataFrame(nfp_sheet.get_all_records())
 
 st.subheader("👤 Trainer Master")
 st.write("Trainer Columns →", trainer_df.columns.tolist())
-st.dataframe(trainer_df[["Emp_ID","Trainer_Name","Phone_Number","Trainer_Type","Designation","Base_Salary","Fixed Pay","Performance_Pay","WP_Resp_Allowance","Status"]].head(10))
+st.dataframe(trainer_df[["Emp_ID","Trainer_Name","Phone_Number","Trainer_Type","Designation","Base_Salary","Fixed_Pay","Performance_Pay","WP_Resp_Allowance","Status"]].head(10))
 
 st.subheader("📄 PT Data")
 st.dataframe(pt_df[["Trainer_Info","Client Name","PT_Charges","Payment_Verified_by_Manager"]].tail(20))
